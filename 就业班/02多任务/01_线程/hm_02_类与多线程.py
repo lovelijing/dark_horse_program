53738447375daf510c3a2592acc903d88636af37
lijing <lj7935482@163.com>
2019-11-24 22:11:02 +0800

    添加文件。

---
+++ 就业班/02多任务/01_线程/hm_02_类与多线程.py
@@ -0,0 +1,16 @@
+import threading
+import time
+
+
+class mythread(threading.Thread):
+    def run(self):
+        for i  in range(3):
+            print('类和线程' + str(time.time()))
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    m1 = mythread()
+    m2 = mythread()
+    m1.start()
+    m2.start()
