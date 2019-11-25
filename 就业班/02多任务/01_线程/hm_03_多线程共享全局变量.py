d86153508592572c26c062ea2e4e8284828097c9
lijing <lj7935482@163.com>
2019-11-25 17:19:52 +0800

    添加文件。

---
+++ 就业班/02多任务/01_线程/hm_03_多线程共享全局变量.py
@@ -0,0 +1,34 @@
+import threading
+# 多线程共有两种方式使用全局变量
+# 1、为直接使用如myname和mytemp两个全局变量
+# 2、使用参数的方法使用全局变量，如mystr
+myname = [1,2]
+mytemp = 123
+mystr = ('ab','cd','efg')
+
+
+def demo1(s1):
+    global  mytemp      # 因为改变了mytemp的指向，所有这里要加global指明mytemp是全局变量
+    mytemp = mytemp + 1
+    myname.append(3)    # 因为没有改变myname的指向，因此不用global
+    print(myname, s1)
+    print(mytemp)
+
+
+def demo2(s1):
+    global mytemp   # 因为改变了mytemp的指向，所有这里要加global指明mytemp是全局变量
+    mytemp += 100
+    myname.append(5)    # 因为没有改变myname的指向，因此不用global
+    print(myname, s1)
+    print(mytemp)
+
+def main():
+    t1 = threading.Thread(target = demo1,args = (mystr,))
+    t2 = threading.Thread(target = demo2,args = (mystr,))
+    t1.start()
+    t2.start()
+
+
+if __name__ == '__main__':
+    main()
+
