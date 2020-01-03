c4d11f32192762b465739b50ac0802ca3b123f0d
lijing <lj7935482@163.com>
2020-01-03 16:26:25 +0800

    增加1个文件。

---
+++ 就业班/02多任务/03_协程/hm_01_迭代器.py
@@ -0,0 +1,29 @@
+class Fiblist(object):
+    def __init__(self,num):
+        self.num = num
+        self.a = 0
+        self.b = 1
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+
+    def __next__(self):
+        if self.count < self.num:
+            num = self.a
+            self.a, self.b = self.b, self.a+self.b
+            self.count += 1
+            return num
+        else:
+            raise StopIteration
+
+
+def main():
+    myfiblist = Fiblist(100)
+    for num in myfiblist:
+        print(num)
+
+
+if __name__ == '__main__':
+    main()
