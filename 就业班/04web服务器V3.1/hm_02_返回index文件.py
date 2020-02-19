576316278f92f6fa4ea7ff0804bc72dec5e58d15
李静 <lj7935482@163.com>
2020-02-19 23:59:14 +0800

    添加两个文件。

---
+++ 就业班/04web服务器V3.1/hm_02_返回index文件.py
@@ -0,0 +1,27 @@
+import socket
+
+
+def http_client(new_socket):
+    get_data = new_socket.recv(1024)
+    print(get_data)
+    file = open("index.html","r")
+    file_data = file.read()
+    file.close()
+    html_data = "http/1.1 200 ok\r\n\r\n <h1>你好！我还在运行着。</h1>"
+    new_socket.send(html_data.encode("GBK"))
+    new_socket.send(file_data.encode("GBK"))
+    new_socket.close()
+
+
+def main():
+    http_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    http_socket.bind(('', 17788))
+    http_socket.listen(128)
+    while True:
+        new_socket, client_address = http_socket.accept()
+        http_client(new_socket)
+    http_socket.close()
+
+
+if __name__ == "__main__":
+    main()
