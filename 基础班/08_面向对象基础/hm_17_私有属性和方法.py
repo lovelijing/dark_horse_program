14132a9c648bfd3a1fbe331bbfa01ae2c57f2042
李静 <lj7935482@163.com>
2020-05-01 00:27:08 +0800

    修改了文件内容。

--- 基础班/08_面向对象基础/hm_17_私有属性和方法.py
+++ 基础班/08_面向对象基础/hm_17_私有属性和方法.py
@@ -8,10 +8,13 @@ class Women:
     def __secret(self):
         # 在对象的方法内部，是可以访问对象的私有属性的
         print("%s 的年龄是 %d" % (self.name, self.__age))
+        # 对象的公有函数可以调用私有函数
+    def show(self):
+        self.__secret()
 
 
 xiaofang = Women("小芳")
-
+xiaofang.show()
 # 私有属性，在外界不能够被直接访问
 # print(xiaofang.__age)
 # 私有方法，同样不允许在外界直接访问
